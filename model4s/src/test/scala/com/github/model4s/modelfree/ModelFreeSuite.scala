63a1c8a6c16f19be308e65ed8a1c93eae609af41
arukavytsia <artem.rukavitsya@gmail.com>
2018-02-12 03:09:28 +0200

    Add more tests for ModelFree

--- model4s/src/test/scala/com/github/model4s/modelfree/ModelFreeSuite.scala
+++ model4s/src/test/scala/com/github/model4s/modelfree/ModelFreeSuite.scala
@@ -5,16 +5,24 @@ import Matchers._
 
 class ModelFreeSuite extends FunSuite {
 
-  test("Valid DAO and DTO models") {
+  test("DAO and DTO models") {
+    @ModelFree case class Person(
+                                  @DAO @DTO id: String,
+                                  @DTO name: String
+                                )
+
+    """Person.DAO("1ee-44-4")""" should compile
+    """Person.DTO("1ee-44-4", "Bill")""" should compile
+
+    """Person.DTO(12, "Bill")""" shouldNot compile
+  }
+
+  test("DAO and DTO models with named parameters") {
     @ModelFree case class Person(
                                   @DAO id: Int,
                                   @DAO @DTO name: String,
                                   @DAO @DTO email: String
                                 )
-
-    """Person.DAO(0, "Steve", "steve@example.com")""" should compile
-    """Person.DTO("Bill", "Bill@example.com")""" should compile
-
     """Person.DAO(id = 0, name = "Steve", email = "steve@example.com")""" should compile
     """Person.DTO(name = "Bill", email = "Bill@example.com")""" should compile
 
@@ -22,4 +30,19 @@ class ModelFreeSuite extends FunSuite {
     """Person.DTO(name = "Bill", emailS = "Bill@example.com")""" shouldNot compile
   }
 
+  test ("Instance of generated class") {
+
+    @ModelFree case class Person(
+                                  @DAO id: Int,
+                                  @DAO @DTO name: String,
+                                  @DAO @DTO email: String
+                                )
+
+    Person.DAO(id = 0, name = "Steve", email = "steve@example.com")
+    Person.DTO(name = "Bill", email = "Bill@example.com")
+
+    """@ModelFree def sqrt(x:Double) = math.sqrt(x)""" shouldNot compile
+    """@ModelFree val age = 30 """" shouldNot compile
+
+  }
 }
